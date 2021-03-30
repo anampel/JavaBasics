66eb8d14b12bdc58b000e80021a301536d6c19ec
Ana Mpel <anampel.am@gmail.com>
2021-03-30 17:37:32 +0300

    Class Patient including comments

--- src/mainpackage/Patient.java
+++ src/mainpackage/Patient.java
@@ -1,12 +1,17 @@
 package mainpackage;
-
+/**
+ *Child of User, concerns patients
+ * */
 public class Patient extends User{
     public String amka;
     public String asfaleia; // (idiotikh/dhmosia)
-
+    /**
+     *Constructor
+     * */
     public Patient(String username, String name, String surname, int usersCounter, String phone, String amka, String asfaleia) {
         super(username, name, surname, usersCounter, phone);
         this.asfaleia = asfaleia;
+        //tha mpei try-catch
         if (usersCounter<1 && amka != "" && amka != null){
             this.amka = amka;
             System.out.println(this.amka);
@@ -15,7 +20,9 @@ public class Patient extends User{
             if (amka == "" || amka == null){System.out.println("Your enter is empty ");}
         }
     }
-
+    /**
+     *Getters & Setters
+     * */
     public String getAmka() {
         return amka;
     }
@@ -31,22 +38,49 @@ public class Patient extends User{
     public void setAsfaleia(String asfaleia) {
         this.asfaleia = asfaleia;
     }
+    /**
+     *Register a user as patient
+     * @param username  The username of the patient
+     * @param name The name of the patient
+     * @param surname The surname of the patient
+     * @param phone The phone of the patient
+     * @param amka The amka of the patient
+     * @param asfaleia The asfaleia of the patient
+     * */
+    public void registration(String username, String name, String surname, String phone, String amka, String asfaleia){
 
-    public void registration(){
+    }
+    /**
+     *Searches appointment per doctor name
+     * @param name  The name of the doctor
+     * @param surnname The surname of the doctor
+     * */
+    public void searchAppointmentPerDoctor(String name, String surnname){
+
+    }
+    /**
+     *Searches appointment per doctor's specialty
+     * @param specialty  The name of the doctor
+     * */
+    public void searchAppointmentPerSpecialty(String specialty){
 
     }
 
-    public void searchAppointmentPerDoctor(){
+    /**
+     *Searches patient's appointments
+     * @param username The username of the patient
+     * @param amka  The amka of the patient
+     * */
+    public void searchMyAppointment(String username,String amka){
 
     }
 
-    public void searchAppointmentPerSpecialty(){
-
-    }
-    public void searchMyAppointment(){
-
-    }
-    public void searchAppointmentHistory(){
+    /**
+     *Searches patient's appointments history
+     * @param username The username of the patient
+     * @param amka  The amka of the patient
+     * */
+    public void searchAppointmentHistory(String username,String amka){
 
     }
 }
