5c1c12a6801b6a4b65c377a76c1a312b640e0e53
Ana Mpel <anampel.am@gmail.com>
2021-03-30 17:43:34 +0300

    Class Doctor including comments

--- src/mainpackage/Doctor.java
+++ src/mainpackage/Doctor.java
@@ -1,13 +1,19 @@
 package mainpackage;
-
+/**
+ *Child of User, concerns Doctors
+ * */
 public class Doctor extends User{
     public String specialty;
 
-
+    /**
+     *Constructor
+     * */
     public Doctor(String username, String name, String surname, int usersCounter, String phone,  String specialty) {
         super(username, name, surname, usersCounter, phone);
     }
-
+    /**
+     *Getters & Setters
+     * */
     public String getSpecialty() {
         return specialty;
     }
@@ -15,8 +21,20 @@ public class Doctor extends User{
     public void setSpecialty(String specialty) {
         this.specialty = specialty;
     }
+    /**
+     *Return the plan of the appointments
+     * @param username The username of the doctor
+     * */
+    public void show_plan(String username){
 
-    //provoli programmatos rantevou: plano
+    }
+    /**
+     *Entry doctor availability for appointments per month
+     * @param username The username of the doctor
+     * @param name The name of the doctor
+     * @param surname The surname of the doctor
+     * */
+    public void available_days(String username, String name, String surname){
 
-    //diathesimes meres gia rantevou
+    }
 }
